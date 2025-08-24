16b6ecf4220577af549c072b256821a764feb331
ARCorbescu <corbescu.robertalexandru@gmail.com>
2025-08-24 19:35:12 +0300

    Update docstring for the fourth exercise and remove hardcoded ssh

--- Bash_Homework/Ex. 4/service_state.sh
+++ Bash_Homework/Ex. 4/service_state.sh
@@ -4,13 +4,15 @@
 # Date Created: 24/08/2025
 # Last Modified: 24/08/2025
 
-# Description
-# The script receives a directory path as an argument
-# If that path points to a valid directory it lists all files and directories with their sizes.
-# If the path is invalid or not a directory, it creates a directory at that path and logs a meesage.
+# Description:
+# This script checks the state of a given systemd service.
+# - If the service is active, it simply reports the status.
+# - If the service is inactive, the user is prompted whether to start it.
+# - If the user accepts, the script starts the service.
+# - If the user declines, the script exits without starting the service.
 
-# Usage
-# directory_handler.sh <directory_path>
+# Usage:
+# ./service_handler.sh <service_name>
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <service_name>"
@@ -24,7 +26,7 @@ if [ $SERVICE_STATE != "active" ]; then
     read -p "Would you like to start the service? y/n: " ANSWER
     if [ $ANSWER == "y" ] || [ $ANSWER == "Y" ]; then
         echo "The service $1 will be started..."
-        sudo systemctl start ssh
+        systemctl start $1
     else
         echo "The service will remain $SERVICE_STATE"
         exit 0
